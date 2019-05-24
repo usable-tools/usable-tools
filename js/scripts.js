06d62df8ed029b4c48dfb294e5b1b00874d985da
squiggy <me@squiggyrubio.net>
2019-05-24 08:29:34 -0700

    Build

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,12 @@
   var body = document.querySelector('body')
   var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
   var menuContainer = document.querySelector('#main-menu-mobile');
-  var social = 
+  var social =
 
   menuTrigger.onclick = function() {
       menuContainer.classList.toggle('open');
       menuTrigger.classList.toggle('is-active')
       body.classList.toggle('lock-scroll')
   }
-
-  // $("#main-menu-mobile .has-dropdown").hover(function() {
-  //   $(this).find('.dropdown').first().stop(true, true).delay(250).slideDown();
-  // }, function() {
-  //   $(this).find('.dropdown').first().stop(true, true).delay(100).slideUp()
-  // });
-
+  
 })();
